1a2ee17228cde6b58275c83e202887a5f2f42130
Gangadhar184 <zenistuig65@gmail.com>
2025-02-03 19:10:40 +0530

    playing with middlewares

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get('/user/:userId',(req,res)=>{
-    // console.log(req.query);
-    console.log(req.params)
-    res.send({abc:"abc",xyz:"xyz"});
+//we generally write middleware using use(becuase use can handle all requests)
+app.use('/admin',(req,res,next)=>{
+    console.log("admin auth is getting checked")
+    const token = "abc";
+    const isAdminAuthorized = token === "abc";
+    if(!isAdminAuthorized){
+        res.status(401).send("Unauthorized request");
+    }else{
+        next();
+    }
 })
+
+app.get("/admin/getAllData",(req,res)=>{
+    //logic
+    res.send("all data sent");
+})
+app.get("/admin/deletUser",(req,res)=>{
+    res.send("deleted a user");
+})
+
 app.listen(7777, ()=>{
     console.log("server is working on this port")
 });
