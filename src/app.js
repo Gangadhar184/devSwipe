0569e7f68be93ef6db0dab067026d21ce324fecd
Gangadhar184 <zenistuig65@gmail.com>
2025-02-03 12:52:21 +0530

    added http methods

--- src/app.js
+++ src/app.js
@@ -1,15 +1,71 @@
 const express = require('express');
 const app = express();
 
+let animeData = {
+    firstName : "Monkey D",
+    lastName : "Luffy",
+    age : "19",
+    country : "goa kingdom"
+}
 
-
-app.use('/test',(req,res)=>{
-    res.send("Luffy is Sungod Nika of this generation");
+app.get('/test',(req,res)=>{
+    res.send(animeData);
 })
 
-app.use('/zoro',(req,res)=>{
-    res.send("Zoro is the king of hell");
+app.post('/test',(req,res) => {
+    // animeData = req.body; 
+    animeData = {
+        firstName : "Monkey D",
+        lastName : "Luffy",
+        age : "19",
+        country : "goa kingdom",
+        role : "captain"
+    }
+    res.send({
+        message : "anime data created successfully",
+        data : animeData
+    })
 })
+
+//using put we can update all body at a time
+app.put('/test',(req,res)=>{
+    animeData = {
+        firstName : "Roronora Zoro",
+        lastName : "zoro",
+        age : "21",
+        country : "east blue",
+        role : "vice-captain"
+    };
+    res.send({
+        message : "anime data replaced successfully",
+        data: animeData
+    })
+})
+
+//we can update individual value using patch
+app.patch('/test',(req,res)=>{
+    animeData.firstName = "Roronora";
+    res.send({
+        message :"first name updated successfully",
+        data : animeData
+    })
+})
+
+// app.patch('/test',(req,res)=>{
+//     animeData = { ...animeData, ...req.body};
+//     res.send({
+//         message : "anime data partially updated successfully",
+//         data : animeData
+//     })
+// });
+
+// app.use('/test',(req,res)=>{
+//     res.send("Luffy is Sungod Nika of this generation");
+// })
+
+// app.use('/zoro',(req,res)=>{
+//     res.send("Zoro is the king of hell");
+// })
 
 //use method will match all the http api calls 
 app.use('/',(req,res)=>{
