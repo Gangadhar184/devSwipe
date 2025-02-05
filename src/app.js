f84088eca9af14a9e792d87be06a52a043b9b70a
Gangadhar184 <zenistuig65@gmail.com>
2025-02-05 16:55:58 +0530

    added signup api to database

--- src/app.js
+++ src/app.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const connectDB = require('./configs/database');
+const User = require('./models/user')
 const app = express();
 
+app.use(express.json());
+
+//singup post request
+app.post('/signup', async (req, res) => {
+    //create instance to the class which is User Model
+
+    const user = new User(req.body);
+    
+    try {
+        await user.save(); //it returns the promise
+        res.send("Post request done successfully")
+    }
+    catch (err) {
+        res.status(400).send("Error in saving userdata: " + err.message)
+    }
+})
+
+//feed api - get/feed get all the users from the database
 
 
 
-connectDB().then(()=>{
+connectDB().then(() => {
     console.log("database connection established");
-    app.listen(7777, ()=>{
+    app.listen(7777, () => {
         console.log("server is working on this port")
     });
-}).catch(()=>{
+}).catch(() => {
     console.log("database cannot connect")
 })
 
